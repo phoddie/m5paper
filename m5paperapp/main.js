a3d3052dc9cadb8ddd4a5421bd7a88c1395fe205
Peter Hoddie <peter@moddable.tech>
2021-10-01 21:29:09 -0700

    JPEG example

--- m5paperapp/main.js
+++ m5paperapp/main.js
@@ -1,4 +1,7 @@
 import Poco from "commodetto/Poco";
+import JPEG from "commodetto/readJPEG";
+import loadJPEG from "commodetto/loadJPEG";
+import Resource from "Resource";
 
 const touch = new device.sensor.Touch({
 	onSample() {
@@ -28,27 +31,54 @@ for (let name in device.peripheral.button) {
 
 screen.clear();
 
-for (let rotation = 0; rotation < 360; rotation += 90) {
-	screen.rotation = rotation;
+doJPEG();
+doRotate();
 
+function doJPEG() {
 	const render = new Poco(screen, {pixels: screen.width * 16});
 
-	const black = render.makeColor(0, 0, 0);
-	const white = render.makeColor(255, 255, 255);
-	const gray = render.makeColor(128, 128, 128);
+	let jpeg = new JPEG(new Resource("office.jpg"));
+	let dx = (render.width - jpeg.width) >> 1, dy = (render.height - jpeg.height) >> 1;
+	dx &= ~3;	// align for driver - could use adaptInvalid
+	
+	let block = jpeg.read();
+	render.begin(block.x + dx, block.y + dy, block.width, block.height);
+	render.drawBitmap(block, block.x + dx, block.y + dy);
 
-	render.begin();
-		render.fillRectangle(black, 0, 0, render.width, render.height);
-
-		render.clip(20, 20, render.width - 40, render.height - 40);
-		render.fillRectangle(white, 0, 0, render.width, render.height);
-
-		render.clip(100, 100, render.width - 200, render.height - 200);
-		render.fillRectangle(gray, 0, 0, render.width, render.height);
-
-		render.clip();
-		render.clip();
-
-		render.fillRectangle(gray, 0, 0, 60, 60);
+	while (block = jpeg.read()) {
+		const x = block.x + dx, y = block.y + dy;
+		render.continue(x, y, block.width, block.height);
+		render.drawBitmap(block, x, y);
+	}
 	render.end();
+}
+
+function doRotate() {
+	for (let rotation = 0; rotation < 360; rotation += 90) {
+		screen.rotation = rotation;
+
+		const render = new Poco(screen, {pixels: screen.width * 16});
+
+		const black = render.makeColor(0, 0, 0);
+		const white = render.makeColor(255, 255, 255);
+		const gray = render.makeColor(128, 128, 128);
+
+		render.begin();
+			render.fillRectangle(black, 0, 0, render.width, render.height);
+
+			render.clip(20, 20, render.width - 40, render.height - 40);
+			render.fillRectangle(white, 0, 0, render.width, render.height);
+
+			render.clip(100, 100, render.width - 200, render.height - 200);
+			render.fillRectangle(gray, 0, 0, render.width, render.height);
+
+			render.clip();
+			render.clip();
+
+			render.fillRectangle(gray, 0, 0, 60, 60);
+		render.end();
+
+		render.close();
+	}
+	screen.rotation = 0;
 }
